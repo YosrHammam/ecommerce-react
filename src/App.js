fc7fb6068108d4cf01aa394e3480c9de8bb3ee9b
YosrHammam <yosrhammam2000@gmail.com>
2023-08-08 18:23:52 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import {Component} from 'react'
+// include all components
+export class App extends Component{
+  state ={};
+  render(){
+    return<>
+     <h1>App Component</h1>
+    </>
+  }
+}
+<App/>
